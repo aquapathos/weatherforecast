83d9f18cd841c33ec3128204c00103c13b3e384a
aquapathos <firedwater@gmail.com>
2024-10-27 10:28:30 +0900

    modified stationsscrapper.py

--- stationscraper.py
+++ stationscraper.py
@@ -3,41 +3,52 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 import re
+
 class AmedasStationScraper(object):
     """
-    scrap = AmedasStationScraper().scrap() で df に取得した観測所情報を格納
+    stations = AmedasStationScraper().stations で stations に取得した観測所情報を格納
     """
-    def __init__(self, encoding='utf-8'):
+    def __init__(self, file_path = None, encoding='utf-8'):
         self.encoding = encoding
-        url = 'https://www.data.jma.go.jp/obd/stats/etrn/select/prefecture00.php?prec_no=&block_no=&year=&month=&day=&view='
-        self.soup = self._get_soup(url, encoding=self.encoding)
-    def _get_soup(self, url: str, encoding: str = None):
+
+        if file_path is None: # csvファイルが与えられていない場合は掻き集めてくる
+            self.stations = self.scrap()
+        else:
+            self.stations = pd.read_csv( file_path, encoding = self.encoding)
+
+    # 指定されたURL用のBeautifulSoupを生成
+    def get_soup(self, url):
         html = requests.get(url)
-        if encoding is not None:
-            html.encoding = encoding
+        html.encoding = self.encoding
         soup = BeautifulSoup(html.text, "html.parser")
         return soup
+
+    # 気象庁データから気象データを取得
     def scrap(self):
         area_list, area_link_list = self.get_all_area_link()
-        stdf = self.get_all_station_link(area_list, area_link_list)
-        return stdf
+        return self.get_all_stations(area_list, area_link_list)
+        
     # エリアごとのページへのリンクを取り出す
     def get_all_area_link(self): 
-        elements = self.soup.find_all('area')  # area（地域名）を取り出す
+        url = 'https://www.data.jma.go.jp/obd/stats/etrn/select/prefecture00.php?prec_no=&block_no=&year=&month=&day=&view='
+        soup = self.get_soup(url)
+        elements = soup.find_all('area')  # area（地域名）を取り出す
         area_list = [element['alt'] for element in elements] # alt（観測所名）を取り出す
         area_link_list = [element['href'] for element in elements] # URLを取り出す
         return area_list, area_link_list
+
     # 与えられたエリアページを訪問し、全観測局のリンクを取り出してデータフレーム化
-    def get_all_station_link(self, area_list, area_link_list) -> pd.DataFrame:
+    def get_all_stations(self, area_list, area_link_list) -> pd.DataFrame:
         dflists = []
         for area, area_link in tqdm(zip(area_list, area_link_list)):
             dflists.append(self.get_station_data(area, area_link))
         stdf = pd.concat(dflists).reset_index(drop=True)
         return self.format_df(stdf)
+
     # 与えられたエリアページを訪問し、全観測局のリンクを取り出してデータフレーム化
     def get_station_data(self, area, area_link) -> pd.DataFrame:
         url = 'https://www.data.jma.go.jp/obd/stats/etrn/select/' + area_link
-        soup = self._get_soup(url, encoding=self.encoding)
+        soup = self.get_soup(url)
         areas = soup.find_all('area')  # area（地域名）をすべて取り出す
         station_list = [area['alt'] for area in areas] # alt（観測所名）を取り出す
         station_link_list = [area['href'].strip( # href=../以降を取り出す
@@ -51,6 +62,7 @@ class AmedasStationScraper(object):
         df = pd.DataFrame(data)
         df['地域'] = area
         return df[['地域', '観測所', 'url', 'info']]
+
     def defaultfind(self, pattern, s, default=None, callback=None):
         cont = re.findall(pattern, s)
         if len(cont) == 0:
@@ -59,6 +71,7 @@ class AmedasStationScraper(object):
             if callback is not None:
                 return callback(cont[0])
             return cont[0]
+
     def format_df(self, df):
         # prec_no
         df['prec_no'] = df.url.apply(
